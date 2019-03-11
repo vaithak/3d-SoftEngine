2f166e791b19825d8edcc24f390a7a6aa78fa045
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2019-03-11 14:07:56 +0530

    Finish Triangle rasterization using scanline conversion

--- SoftEngine.js
+++ SoftEngine.js
@@ -28,7 +28,7 @@ SoftEngine.Device = class {
 
     LoadJSONFile(filename, callback) {
         let currObj = this;
-        $.getJSON( "monkey.babylon", function(data) {
+        $.getJSON( filename, function(data) {
             callback(currObj.createMeshes(data))
         });
     }
@@ -37,7 +37,7 @@ SoftEngine.Device = class {
     createMeshes(jsonObject) {
         var meshes = [];
         for(var meshIndex = 0; meshIndex < jsonObject.meshes.length; meshIndex++) {
-            var verticesArray = jsonObject.meshes[meshIndex].vertices;
+            var verticesArray = (jsonObject.meshes[meshIndex].vertices) || (jsonObject.meshes[meshIndex].positions);
             // Faces
             var indicesArray = jsonObject.meshes[meshIndex].indices;
 
@@ -173,6 +173,98 @@ SoftEngine.Device = class {
         return (new BABYLON.Vector2(x, y));
     }
 
+    // Rasterization of Triangles using Scanline conversion
+    processScanLine (y, pa, pb, pc, pd, color) {
+        let gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
+        let gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+    
+        let sx = pa.x + (pb.x - pa.x)*gradient1;
+        let ex = pc.x + (pd.x - pc.x)*gradient2;
+    
+        // drawing a line from left (sx) to right (ex) 
+        for(var x = sx; x < ex; x++) {
+            this.drawPoint(new BABYLON.Vector2(x, y), color);
+        }
+    }
+
+    drawTriangle (p1, p2, p3, color) {
+        // Sorting the points in order to always have this order on screen p1, p2 & p3
+        // with p1 always up (thus having the Y the lowest possible to be near the top screen)
+        // then p2 between p1 & p3
+        if(p1.y > p2.y) {
+            var temp = p2;
+            p2 = p1;
+            p1 = temp;
+        }
+        if(p2.y > p3.y) {
+            var temp = p2;
+            p2 = p3;
+            p3 = temp;
+        }
+        if(p1.y > p2.y) {
+            var temp = p2;
+            p2 = p1;
+            p1 = temp;
+        }
+    
+        // Computing slopes
+        let invSlopeP1P2, invSlopeP1P3;
+        if(p2.y != p1.y) {
+            invSlopeP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+        } else {
+            // For easy calculation
+            invSlopeP1P2 = 0;
+        }
+    
+        if(p3.x != p1.x) {
+            invSlopeP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+        } else {
+            // For easy calculation
+            invSlopeP1P3 = 0;
+        }
+    
+        // Case where triangles are like that:
+        // P1
+        // -
+        // -- 
+        // - -
+        // -  -
+        // -   - P2
+        // -  -
+        // - -
+        // -
+        // P3
+        if(invSlopeP1P2 > invSlopeP1P3) {
+            for(var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                if(y < p2.y) {
+                    this.processScanLine(y, p1, p3, p1, p2, color);
+                } else {
+                    this.processScanLine(y, p1, p3, p2, p3, color);
+                }
+            }
+        }
+        // Case where triangles are like that:
+        //       P1
+        //        -
+        //       -- 
+        //      - -
+        //     -  -
+        // P2 -   - 
+        //     -  -
+        //      - -
+        //        -
+        //       P3
+        else {
+            for(var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+                if(y < p2.y) {
+                    this.processScanLine(y, p1, p2, p1, p3, color);
+                } else {
+                    this.processScanLine(y, p2, p3, p1, p3, color);
+                }
+            }
+        }
+    }
+
     // The main method of the engine that re-compute each vertex projection during each frame
     render (camera, meshes) {
         // To understand this part, please read the prerequisites resources
@@ -201,9 +293,14 @@ SoftEngine.Device = class {
                 var pixelB = this.projectPointOnScreen(vertexB, transformMatrix);
                 var pixelC = this.projectPointOnScreen(vertexC, transformMatrix);
 
-                this.breshnamDrawLine(pixelA, pixelB);
-                this.breshnamDrawLine(pixelB, pixelC);
-                this.breshnamDrawLine(pixelC, pixelA);
+                // FOR WIREFRAME RENDERING
+                // this.breshnamDrawLine(pixelA, pixelB);
+                // this.breshnamDrawLine(pixelB, pixelC);
+                // this.breshnamDrawLine(pixelC, pixelA);
+
+                // FOR TRIANGLE RASTERIZATION
+                var color = 0.25 + (indexFaces / cMesh.Faces.length) * 0.75;
+                this.drawTriangle(pixelA, pixelB, pixelC, new BABYLON.Color4(color, color, color, 1));
             });
         });
     }
